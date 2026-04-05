0e9e525b83a42114511bb8ba2bc4734c41715aad
anujedits42-byte <anujedits42@gmail.com>
2026-04-05 18:29:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,20 +9,20 @@ import random
 # Telegram Channel @CantarellaBots
 #Supoort group @rexbotschat
 # Bot Configuration
-API_TOKEN = os.environ.get("API_TOKEN", "")
+API_TOKEN = os.environ.get("API_TOKEN", "7820299629:AAGs1AKd-IJZFi7I6K4Yoo5Yps8i-FiUphg")
 # CantarellaBots
 # Don't Remove Credit
 # Telegram Channel @CantarellaBots
 #Supoort group @rexbotschat
 # MongoDB
-MONGO_URL = os.environ.get("MONGO_URL", "")
-DB_NAME = "thumbnail_bot"
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
+DB_NAME = "Anujedit"
 # CantarellaBots
 # Don't Remove Credit
 # Telegram Channel @CantarellaBots
 #Supoort group @rexbotschat
 # Owner/Admin
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7892805795"))
 # CantarellaBots
 # Don't Remove Credit
 # Telegram Channel @CantarellaBots
@@ -37,9 +37,9 @@ START_PICS = [
 # Don't Remove Credit
 # Telegram Channel @CantarellaBots
 #Supoort group @rexbotschat
-CHANNEL_URL = os.environ.get("CHANNEL_URL", "https://t.me/cantarellabots")
-DEV_URL = os.environ.get("DEV_URL", "https://t.me/cantarella_wuwa")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))  # e.g., -100xxxxxxxxxxxx
+CHANNEL_URL = os.environ.get("CHANNEL_URL", "https://t.me/log_channel_a")
+DEV_URL = os.environ.get("DEV_URL", "https://t.me/anujedits76")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003515041061"))  # e.g., -100xxxxxxxxxxxx
 # CantarellaBots
 # Don't Remove Credit
 # Telegram Channel @CantarellaBots
